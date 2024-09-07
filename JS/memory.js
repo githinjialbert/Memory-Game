d4d6aebeb4cf42fe810f331b6ea8bc8462b85afe
githinjialbert <githinjialbert001@gmail.com>
2024-09-07 15:10:35 +0300

    updating the logic behind the createBoard function

--- JS/memory.js
+++ JS/memory.js
@@ -1,23 +1,23 @@
-const cards = ["1up", "blueshell", "bobomb", "bulletbill", "coin", "goomba", "luigi", 
-    "mario", "mushroom", "peach", "question", "star", "thwomp", "wario"];
-
-const board = [];
+const cards = ["1up", "blueshell", "bobomb", "bulletbill", "coin", "goomba", "luigi", "mario", "mushroom", "peach", "question", "star", "thwomp", "wario"];
 const rows = 6;
 const columns = 4;
-let currentTile;
-let otherTile;
-
-const myBoard = document.getElementById("board");
+let flippedCards = [];
+let hiddenImage = "./images/question.png"
 
 window.onload = () => {
     startGame();
+}
 
+const startGame = () => {
+    createBoard();
+}
 
-    window.setInterval(() => {
-
-        flipCard();
-        matchCard();
-        endGame();
-        
-    }, 100);
+const createBoard = () => {
+    const myBoard = document.getElementById("board");
+    const tileValues = [...cards, ...cards];
+    tileValues = shuffleArray(tileValues);
+    
+    for (let i = 0; i < rows * columns; i++) {
+        const tile = document.createElement("img");
+    }
 }
